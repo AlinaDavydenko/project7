7b2922716780218643d69b245a89e3055477909b
Alina Davydenko <Alinadavydenko620@gmail.com>
2024-08-26 23:28:53 +0300

    убран вызов функций в masks.py

--- src/masks.py
+++ src/masks.py
@@ -7,14 +7,3 @@ def get_mask_account(account_number: str) -> str:
     """ принимает на вход номер счета и возвращает его маску """
     return f'**{account_number[-4:]}'  # выход функции
 
-
-card_number = str(input('Введите номер карты.'))
-account_number = str(input('Введите номер счёта.'))
-
-
-mask_card_number = get_mask_card_number(card_number)  # возвращает маску номера карты
-mask_account = get_mask_account(account_number)  # возвращает маску номера счёта
-
-
-print(mask_card_number)
-print(mask_account)
---
+++ tests/my_tests.py
@@ -0,0 +1,4 @@
+a = 'a4567A4456'
+card = 'Maestro 1596837868705199'
+print(card.split(' '))
+
